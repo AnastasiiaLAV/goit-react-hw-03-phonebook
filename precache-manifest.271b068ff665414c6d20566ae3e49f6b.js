098511fd2e6253348b4178373c96afc1ac81013d
AnastasiiaLAV <95190359+AnastasiiaLAV@users.noreply.github.com>
2022-08-04 17:15:24 +0000

    Deploying to gh-pages from @ AnastasiiaLAV/goit-react-hw-03-phonebook@57caa107f0b668154b3ab66fed29c3f5bf4f83a9 🚀

---
+++ precache-manifest.271b068ff665414c6d20566ae3e49f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b87cc707190439abde57",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.2adfd53e.chunk.css"
+  },
+  {
+    "revision": "b87cc707190439abde57",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.b87cc707.chunk.js"
+  },
+  {
+    "revision": "d454aaf41af633c0b21f",
+    "url": "/goit-react-hw-03-phonebook/static/css/1.f927bbe2.chunk.css"
+  },
+  {
+    "revision": "d454aaf41af633c0b21f",
+    "url": "/goit-react-hw-03-phonebook/static/js/1.d454aaf4.chunk.js"
+  },
+  {
+    "revision": "4cf10fc43dcb413c0b07",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime~main.4cf10fc4.js"
+  },
+  {
+    "revision": "c66c34b32b76da2a6786dc850c8b8ad5",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  }
+];
